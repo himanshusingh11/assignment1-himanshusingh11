94d99f79fde192751791cbcaa6b51d3a199fdf43
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-18 16:27:57 +0530

    assingnment done Himanshu Singh section M

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -3,27 +3,18 @@
  * @version 1.0
  * */
 package main;
-
-// import java.util.Scanner;
-// uncomment the above line to use input in your program.
-
 import java.util.Scanner;
-
 public class Gravity {
+	public static double speed;     // speed is given in meters per second
+	public static double distance;      //distance is in meters
 
+	public static void main(String[] args) {
+		float g = 9.8f;
+		double time;//time is taken in seconds
+		Scanner sc = new Scanner(System.in);
+		time = sc.nextDouble();
 
-
-
-		public static double speed;     // speed is given in meters per second
-		public static double distance;      //distance is in meters
-
-		public static void main(String[] args) {
-			float g = 9.8f;
-			double time;//time is taken in seconds
-			Scanner sc = new Scanner(System.in);
-			time = sc.nextDouble();
-
-			System.out.println("the speed of the object at time t seconds after its release is  : " + (g * time) + "m/s" + "the distance the object has travelled in those t seconds after its release is : " + 0.5 * g * Math.pow(time, 2) + "meters");
+		System.out.println("the speed of the object at time t seconds after its release is  : " + (g * time) + "m/s" + "the distance the object has travelled in those t seconds after its release is : " + 0.5 * g * Math.pow(time, 2) + "meters");
 
 
 		}
