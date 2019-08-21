6f5b5eabd85053ff2617033e155abcfe8b58553d
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-22 02:12:25 +0530

    integer is taken as a input

--- src/question3/solution3.java
+++ src/question3/solution3.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class solution3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
 
 
     }
