d09972b39e06faea01d47ce592521a82b1e43460
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-21 17:51:17 +0530

    added comma after welcome

---
+++ src/question2/solution2.java
@@ -0,0 +1,14 @@
+package question2;
+
+public class solution2 {
+    import java.util.Scanner;  //   to import the Scanner class inside your program
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);  // to create an object of the Scanner class
+            // write your code here
+            scanner.close();  // write this line at the end of the main method
+        }
+    }
+
+
