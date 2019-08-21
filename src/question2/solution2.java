be0f864fa2a883288786a1e2ac66ee650d669510
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-22 02:04:24 +0530

    string object is created

--- src/question2/solution2.java
+++ src/question2/solution2.java
@@ -1,14 +1,13 @@
 package question2;
-
 import java.util.Scanner;
 
 public class solution2 {
-    public class Main {
+
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);  // to create an object of the Scanner class
 
 
-            scanner.close();  // write this line at the end of the main method
+            scanner.close();
         }
     }
 
