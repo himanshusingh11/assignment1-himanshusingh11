8a99b58ec0503a1ec768b236c3c2723683f7cd21
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-21 17:46:35 +0530

    added scanner class and input method scanner class closes

---
+++ src/question1/solution1.java
@@ -0,0 +1,13 @@
+package question1;
+
+import java.util.Scanner;
+
+public class solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // to create an object of the Scanner class
+        // write your code here
+        String s = scanner.nextLine();
+        System.out.println("Welcome " + " " + s + "!");
+        scanner.close();
+    }
+}
