e77508f8d0312814f2013cc4343868942cd7b299
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-21 17:08:35 +0530

    added sc.close()

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -12,10 +12,11 @@ public class Gravity {
 		float g = 9.8f;
 		double time;//time is taken in seconds
 		Scanner sc = new Scanner(System.in);
+
 		time = sc.nextDouble();
 
 		System.out.println("the speed of the object at time t seconds after its release is  : " + (g * time) + "m/s" + "the distance the object has travelled in those t seconds after its release is : " + 0.5 * g * Math.pow(time, 2) + "meters");
-
+		sc.close();
 
 		}
 	}
