0f78f4a0dc7e454dcfee90519de82f0c5a255f60
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-22 16:39:05 +0530

    scanner class is created

---
+++ src/question4/solution4.java
@@ -0,0 +1,11 @@
+package question4;
+
+import java.util.Scanner;
+
+public class solution4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
