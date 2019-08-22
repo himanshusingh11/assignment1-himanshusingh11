86ff030dee8272b83e0c04fd7173f997477e1a69
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-22 16:34:44 +0530

    perform various operations on integers

--- src/question3/solution3.java
+++ src/question3/solution3.java
@@ -6,6 +6,14 @@ public class solution3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+        System.out.println(x + " - " + y + " = " + (x - y));
+        System.out.println(x + " * " + y + " = " + (x * y));
+        System.out.println(x + " / " + y + " = " + (x / y));
+        System.out.println(x + " % " + y + " = " + (x % y));
+        scanner.close();
 
 
 
