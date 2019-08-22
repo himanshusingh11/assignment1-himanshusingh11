bbd29b4a18ae61888352d01afbf5c282e174be80
himanshusingh11 <mail.himanshu.singh_iot18@gla.ac.in>
2019-08-22 16:43:14 +0530

    integer is taken as input

--- src/question4/solution4.java
+++ src/question4/solution4.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class solution4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x > 0) {
+            System.out.println("Positive");
+        } else if (x < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
 
 
     }
